243adc371f426990f67a5aa9518a0f9ee7597bf2
Simone Francesco Fornasari <73161805+sffornasari@users.noreply.github.com>
2022-07-06 21:25:41 +0200

    Create compute_differences.py

---
+++ analysis/compute_differences.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import json
+import analysis_utils as autils
+
+otopath = ''
+csvpath = ''
+
+P = [0.0625, 0.125, 0.25, 0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0]
+otofiles = {pi: glob.glob(f'oto_P_ts/oto*{pi:.2e}*npz') for pi in P}
+
+dn = {pi:{} for pi in P}
+wdwe = {pi:{} for pi in P}
+ws = {pi:{} for pi in P}
+
+for Pk, otok in otofiles.items():
+    otos = [np.load(otok[i]) for i in [0,2]]
+    moto = autils.merge_otos(otos)
+    dn[Pk] = autils.day_night_md(moto)
+    ws[Pk] = autils.wint_summ(moto)
+    _, __, wdwe[Pk] = autils.wd_we(moto)
+    
+with open('day_night.json', 'w') as fp:
+    json.dump(dn, fp)
+with open('wd_we.json', 'w') as fp:
+    json.dump(wdwe, fp)
+with open('wint_summ.json', 'w') as fp:
+    json.dump(ws, fp)
