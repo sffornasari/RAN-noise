84a1f8cdbb8ec69e9a5fb8c50983522c8c9f7dc9
Simone Francesco Fornasari <73161805+sffornasari@users.noreply.github.com>
2022-07-07 09:07:47 +0200

    Update compute_differences.py

--- analysis/compute_differences.py
+++ analysis/compute_differences.py
@@ -5,6 +5,7 @@ import analysis_utils as autils
 
 otopath = ''
 csvpath = ''
+outpath = './'
 
 P = [0.0625, 0.125, 0.25, 0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0]
 otofiles = {pi: glob.glob(f'oto_P_ts/oto*{pi:.2e}*npz') for pi in P}
@@ -20,13 +21,13 @@ for Pk, otok in otofiles.items():
     _, __, wdwe[Pk] = autils.wd_we(moto)
     md, s2_5 = autils.yearly_statistics(moto)
     
-with open('day_night.json', 'w') as fp:
+with open(f'{outpath}/day_night.json', 'w') as fp:
     json.dump(dn, fp)
-with open('wd_we.json', 'w') as fp:
+with open(f'{outpath}/wd_we.json', 'w') as fp:
     json.dump(wdwe, fp)
-with open('wint_summ.json', 'w') as fp:
+with open(f'{outpath}/wint_summ.json', 'w') as fp:
     json.dump(ws, fp)
-with open('yearly_median.json', 'w') as fp:
+with open(f'{outpath}/yearly_median.json', 'w') as fp:
     json.dump(md, fp)
-with open('yearly_2_5_statistics.json', 'w') as fp:
+with open(f'{outpath}/yearly_2_5_statistics.json', 'w') as fp:
     json.dump(s2_5, fp)
