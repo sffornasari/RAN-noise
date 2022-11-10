31665cec2de868a32f02780847b0afecaa1f394b
Deniz Ertuncay <dertuncay@gmail.com>
2022-11-10 11:17:05 +0100

    Update and rename covid_general.py to Codes/covid_general.py

--- Codes/covid_general.py
+++ Codes/covid_general.py
@@ -35,14 +35,12 @@ def draw_map(fig,ax,stname,stlos,stlas,data,vmin,vmax):
 
 
 # Opening JSON file
-f = open('../../DBs/JSON_Final/yearly_median_ext_covid_diff.json')
-# returns JSON object as 
-# a dictionary
+f = open('../DBs/JSON_Final/yearly_median_ext_covid_diff.json')
 data = json.load(f)
 
 
 # Read Station Info
-dpc_db = pd.read_csv('../../DBs/station_attributes.csv')
+dpc_db = pd.read_csv('../DBs/station_attributes.csv')
 
 
 # Define Figure
@@ -50,16 +48,10 @@ dpc_db = pd.read_csv('../../DBs/station_attributes.csv')
 stamen_terrain = cimgt.Stamen('terrain-background')
 fig,axs = plt.subplots(2,3, figsize=(16, 10), facecolor='w', edgecolor='k',subplot_kw={'projection': stamen_terrain.crs}, gridspec_kw = {'wspace':0, 'hspace':0.1})
 axs = axs.ravel()
-# fig.delaxes(axs[-1]) #The indexing is zero-based here
 # Annotation
 annotations = list(string.ascii_lowercase)
 
-# periods = list(data.keys())[:-10]
 periods = ['0.0992','0.25','0.5','1.0','2.0','5.04']
-
-# vmins = [-135,-135,-135,-135,-135,-135,-129.5,-128.2,-124.1]
-# vmaxs = [-91.5,-101.3,-114.1,-120.0,-112.1,-97.6,-104.4,-102.5,-98.1]
-
 
 stas = []; difs = []; pers = [];
 total_db = pd.DataFrame(columns=['Period', 'Station', 'Value'])
@@ -77,7 +69,6 @@ for per_idx, period in enumerate(periods):
 		dif.append(val)
 		total_db = total_db.append(pd.Series([float(period),sta,val], index=['Period', 'Station', 'Value']), ignore_index=True) 
 
-	# #print(dif)
 	dif = np.array(dif)
 	abs_dif = np.absolute(dif)
 	vmax = np.nanpercentile(abs_dif, 95)
@@ -90,4 +81,4 @@ for period in periods:
 	print(per_db.sort_values(by=['Value']))
 
 axs[-1].text(0.45, -0.05, 'Red = Non Covid Noisier', transform=axs[per_idx].transAxes, size=8)
-plt.savefig('../../Figures/Covid_Dif2/coviddif.png',dpi=300, bbox_inches='tight')
+plt.savefig('../Figures/Figure 9.png',dpi=300, bbox_inches='tight')
